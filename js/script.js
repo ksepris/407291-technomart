d3db72cfe43a2ec9277378fca7d4a5e5fd56b829
ksepris <ksepris@gmail.com>
2017-05-12 19:49:55 +0300

    Исправления

--- js/script.js
+++ js/script.js
@@ -1,6 +1,7 @@
 var link = document.querySelector(".company_contacts .main_btn");
 var popup = document.querySelector(".modal_content");
 var close = popup.querySelector(".modal_content_close");
+
 link.addEventListener("click", function(event) {
   event.preventDefault();
   popup.classList.add("modal_content_show");
@@ -8,4 +9,17 @@ link.addEventListener("click", function(event) {
 close.addEventListener("click", function(event) {
         event.preventDefault();
         popup.classList.remove("modal_content_show");
+      });
+
+var link_map = document.querySelector(".map_open");
+var popup_map = document.querySelector(".modal_content_map");
+var close_map = popup_map.querySelector(".modal_content_close");
+
+link_map.addEventListener("click", function(event) {
+  event.preventDefault();
+  popup_map.classList.add("modal_content_show");
+      });
+close_map.addEventListener("click", function(event) {
+        event.preventDefault();
+        popup_map.classList.remove("modal_content_show");
       });

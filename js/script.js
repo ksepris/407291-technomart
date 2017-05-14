afeb5ebde2359dd4a7fb1b452db7593393d0f18c
ksepris <ksepris@gmail.com>
2017-05-14 13:07:15 +0300

    final version 2_1

--- js/script.js
+++ js/script.js
@@ -1,38 +1,56 @@
-var link = document.querySelector(".company_contacts .main_btn");
-var popup = document.querySelector(".modal_content");
-var close = popup.querySelector(".modal_content_close");
+if (document.querySelector('.modal_content') !== null) {
+  var link = document.querySelector(".company_contacts .main_btn");
+  var popup = document.querySelector(".modal_content");
+  var close = popup.querySelector(".modal_content_close");
 
-link.addEventListener("click", function(event) {
-  event.preventDefault();
-  popup.classList.add("modal_content_show");
-      });
-close.addEventListener("click", function(event) {
-        event.preventDefault();
-        popup.classList.remove("modal_content_show");
-      });
+  link.addEventListener("click", function (event) {
+    event.preventDefault();
+    popup
+      .classList
+      .add("modal_content_show");
+  });
+  close.addEventListener("click", function (event) {
+    event.preventDefault();
+    popup
+      .classList
+      .remove("modal_content_show");
+  });
+}
 
-var link_map = document.querySelector(".map_open");
-var popup_map = document.querySelector(".modal_content_map");
-var close_map = popup_map.querySelector(".modal_content_close");
+if (document.querySelector('.modal_content_map') !== null) {
+  var link_map = document.querySelector(".map_open");
+  var popup_map = document.querySelector(".modal_content_map");
+  var close_map = popup_map.querySelector(".modal_content_close");
 
-link_map.addEventListener("click", function(event) {
-  event.preventDefault();
-  popup_map.classList.add("modal_content_show");
-      });
-close_map.addEventListener("click", function(event) {
-        event.preventDefault();
-        popup_map.classList.remove("modal_content_show");
-      });
+  link_map.addEventListener("click", function (event) {
+    event.preventDefault();
+    popup_map
+      .classList
+      .add("modal_content_show");
+  });
+  close_map.addEventListener("click", function (event) {
+    event.preventDefault();
+    popup_map
+      .classList
+      .remove("modal_content_show");
+  });
+}
 
-var link_buy = document.querySelector(".buy_btn");
-var popup_buy = document.querySelector(".modal_content_buy");
-var close_buy = popup_buy.querySelector(".modal_content_close");
+if (document.querySelector('.modal_content_buy') !== null) {
+  var link_buy = document.querySelector(".buy_btn");
+  var popup_buy = document.querySelector(".modal_content_buy");
+  var close_buy = popup_buy.querySelector(".modal_content_close");
 
-link.addEventListener("click", function(event) {
-  event.preventDefault();
-  popup_buy.classList.add("modal_content_show");
-      });
-close_buy.addEventListener("click", function(event) {
-        event.preventDefault();
-        popup_buy.classList.remove("modal_content_show");
-      });
+  link_buy.addEventListener("click", function (event) {
+    event.preventDefault();
+    popup_buy
+      .classList
+      .add("modal_content_show");
+  });
+  close_buy.addEventListener("click", function (event) {
+    event.preventDefault();
+    popup_buy
+      .classList
+      .remove("modal_content_show");
+  });
+}
